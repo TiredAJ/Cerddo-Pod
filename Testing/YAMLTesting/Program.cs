154b27e644ad4896319c6fa5e2fdad3debfc687f
AJ <aletha.e.jones@gmail.com>
2024-08-03 17:27:03 +0100

    yaml testing

--- Testing/YAMLTesting/Program.cs
+++ Testing/YAMLTesting/Program.cs
@@ -1,29 +1,65 @@
 ﻿using System.Diagnostics;
 using System.Reflection.Metadata.Ecma335;
 using System.Text.Json.Serialization.Metadata;
+using YamlDotNet;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+using YamlDotNet.Serialization.TypeResolvers;
 
 namespace YAMLTesting;
 
 class Program
 {
-    private Song Temp = new Song()
+    private static Song TempSong = new Song
     {
         Name = "Up & Up",
         Album = "A Head Full of Dreams",
         ArtistName = "Coldplay",
         TrackLength = 405L,
-        Effects = [ new Fade(345L, 571L), new VolumeUp()]
-    }
+        Effects =
+        [
+            new Fade(345L,
+                571L),
+            new VolumeUp(5)
+        ],
+        OtherAdjustments = [new AdjustedEnds(20L, Int64.MaxValue/4)],
+    };
     
     static void Main(string[] args)
-    { 
+    {
+        Song? DeserialisedSong = null;
         
+        var Serialiser = new SerializerBuilder()
+                                            .WithNamingConvention(PascalCaseNamingConvention.Instance)
+                                            .WithTypeResolver(new DynamicTypeResolver())
+                                            .WithTagMapping("!Fade", typeof(Fade))
+                                            .WithTagMapping("!VolumeUp", typeof(VolumeUp))
+                                            .WithTagMapping("!AdjustedEnds", typeof(AdjustedEnds))
+                                            .Build();
+
+        var YAMLOutput = Serialiser.Serialize(TempSong);
+        Console.Write(YAMLOutput);
+        
+        DeserialisedSong = new DeserializerBuilder()
+                                .WithNamingConvention(PascalCaseNamingConvention.Instance)
+                                .WithTypeResolver(new DynamicTypeResolver())
+                                .WithTagMapping("!Fade", typeof(Fade))
+                                .WithTagMapping("!VolumeUp", typeof(VolumeUp))
+                                .WithTagMapping("!AdjustedEnds", typeof(AdjustedEnds))
+                                .Build()
+                                .Deserialize<Song>(YAMLOutput);
+        
+        Console.WriteLine("\n");
+        Console.WriteLine($"Song: {DeserialisedSong.Name}");
+        
+        DeserialisedSong.RunEffects();
     }
 }
 
 public interface IAdjustment
 {
-    public void RunAdjustment();
+    public virtual void RunAdjustment()
+    { Debug.WriteLine("Running base adjustment!"); }
 }
 
 abstract class Effect : IAdjustment
@@ -31,13 +67,19 @@ abstract class Effect : IAdjustment
     public long StartPos { get; set; }
     public long EndPos { get; set; }
 
-    public void RunAdjustment()
-    { Debug.WriteLine("Running base adjustment!"); }
+    public virtual void RunAdjustment()
+    { Debug.WriteLine("Running base effect!"); }
 }
 
 class AdjustedEnds : IAdjustment
 {
-    public AdjustedEnds(long _NewStartPos, long _NewEndPos, ref Song _AffectedSong)
+    public AdjustedEnds()
+    {
+        StartPos = 0L;
+        EndPos = long.MaxValue;
+    }
+    
+    public AdjustedEnds(long _NewStartPos, long _NewEndPos)
     {
         StartPos = _NewStartPos;
         EndPos = _NewEndPos;
@@ -54,6 +96,12 @@ class AdjustedEnds : IAdjustment
 
 class Fade : Effect
 {
+    public Fade()
+    {
+        StartPos = 0L;
+        EndPos = long.MaxValue;
+    }
+    
     public Fade(long _StartPos, long _EndPos)
     {
         StartPos = _StartPos;
@@ -68,6 +116,9 @@ class VolumeUp : Effect
 {
     public int PercentIncrease { get; set; }
 
+    public VolumeUp()
+    { PercentIncrease = 0; }
+    
     public VolumeUp(int _PercentIncrease)
     { PercentIncrease = _PercentIncrease; }
     
@@ -94,19 +145,24 @@ class Song
     
     public void RunEffects()
     {
-        
+        foreach (var Efct in Effects)
+        { Efct.RunAdjustment(); }
     }
 
-    public async TriggerAdjustments()
+    public async Task TriggerAdjustments()
     {//trigger at somepoint after _OtherAdjustments is set
 
-        foreach (var Adj in _OtherAdjustments)
+        (IAdjustment Adjustment, bool Adjusted) Adj;
+        
+        for (int i = 0; i < _OtherAdjustments.Count; i++)
         {
+            Adj = _OtherAdjustments[i]; 
+            
             if (!Adj.Adjusted)
             {
                 Adj.Adjusted = true;
+                Adj.Adjustment.RunAdjustment();
             }
         }
-        
     }
 }
